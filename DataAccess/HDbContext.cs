9b6fc700c88b00c2706da93ac33cae94d5cbb260
Edleron <Edleron@hotmail.com>
2024-02-08 22:59:59 +0300

    add : data accsess

---
+++ DataAccess/HDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class MyDataBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
