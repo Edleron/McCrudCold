cad246f61230bc8a76b74d89a427e582bfaec962
Edleron <Edleron@hotmail.com>
2024-02-08 22:52:13 +0300

    add : entities

---
+++ Entities/Hotels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class Hotels
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public int Name { get; set; }
+
+        [StringLength(50)]
+        public int City { get; set; }
+    }
+}
