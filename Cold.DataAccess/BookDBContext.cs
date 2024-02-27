bd0e791f99d37f84bf471365449f3b9e7e7e59f8
Edleron <Edleron@hotmail.com>
2024-02-27 20:33:52 +0300

    add : data acsess

---
+++ Cold.DataAccess/BookDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cold.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cold.DataAccess
+{
+    public class BookDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-79VLTFV; Database=ColdDB;uid=sa;pwd=10ed10;");
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
