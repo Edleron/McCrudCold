af947f2044ad7c4b824f39ff34002097462201fc
Edleron <Edleron@hotmail.com>
2024-02-27 20:34:28 +0300

    add : abstract

---
+++ Cold.Business/Abstract/IBookService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cold.Entities;
+
+namespace Cold.Business.Abstract
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooks();
+
+        Book GetBookById(int id);
+
+        Book CreateBook(Book book);
+
+        Book UpdateBook(Book book);
+
+        void DeleteBook(int id);
+    }
+}
