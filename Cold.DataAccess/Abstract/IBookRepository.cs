222ede1720dc943d5fb46af71303b5edc3887424
Edleron <Edleron@hotmail.com>
2024-02-27 20:33:43 +0300

    add : abstract

---
+++ Cold.DataAccess/Abstract/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using Cold.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cold.DataAccess.Abstract
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAllBooks();
+
+        Book GetBookById(int id);
+
+        Book CreateBook(Book book);
+
+        Book UpdateBook(Book book);
+
+        void DeleteBook(int id);
+    }
+}
